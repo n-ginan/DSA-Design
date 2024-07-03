d85a42e57652f49d5b9afece0a5368c7640bc828
papsykle <toramgarcia@gmail.com>
2024-07-04 01:14:39 +0800

    second commit

--- Singly.kt
+++ Singly.kt
@@ -1,4 +1,12 @@
 data class Singly<T:Any> (
     var value: T,
     var nextNode: Singly<T>? = null
-)
+) {
+    override fun toString() : String {
+        return if (nextNode != null) {
+            "$value -> ${nextNode.toString()}"
+        } else {
+            "$value"
+        }
+    }
+}
